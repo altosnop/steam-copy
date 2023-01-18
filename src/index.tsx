4a5343340e9d159f2f514687e3997717baed6da9
Oleksii Larchenko <oleksii.larchen@gmail.com>
2023-01-18 21:17:58 +0200

    Change router to Hash

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/configureStore';
 import GlobalStyles from './global';
@@ -16,10 +16,10 @@ const root = ReactDOM.createRoot(
 root.render(
 	<Provider store={store}>
 		<PersistGate loading={null} persistor={persistor}>
-			<BrowserRouter>
+			<HashRouter>
 				<GlobalStyles />
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</PersistGate>
 	</Provider>
 );
